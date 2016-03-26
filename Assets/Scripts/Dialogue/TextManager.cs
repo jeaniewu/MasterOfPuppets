ed2cc5e60f429309784c738ebc97e1b6b0316f9b
maxahn <ahnmaximus@gmail.com>
2016-03-26 16:09:29 -0700

    progress on text array

--- Assets/Scripts/Dialogue/TextManager.cs
+++ Assets/Scripts/Dialogue/TextManager.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TestManager : MonoBehaviour {
+public class TextManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
--- Assets/Scripts/dialogueTrigger.cs
+++ Assets/Scripts/dialogueTrigger.cs
@@ -3,15 +3,21 @@ using System.Collections;
 
 public class dialogueTrigger : Trigger {
 
-	public TextAsset theText;
+	//public TextAsset theText;
 
 	public int startLine;
 	public int endLine;
 
 	public TextBoxManager theTextBox;
 
+	public TextAsset[] texts;
+	public TextAsset theText;
+	public int pointer;
+	public bool loop;
+	public int size;
 	// Use this for initialization
 	void Start () {
+		pointer = 0;
 		theTextBox = FindObjectOfType < TextBoxManager >();
 	}
 	
@@ -21,10 +27,24 @@ public class dialogueTrigger : Trigger {
 	}
 
 	public override void switchTrigger(){
-		theTextBox.ReloadScript(theText);
-		theTextBox.currentLine = startLine;
-		theTextBox.endAtLine = endLine;
-		Invoke("enable", 0.1f);
+		//Debug.Log ("size: " + size);
+		Debug.Log ("pointer: " + pointer);
+
+		if (pointer < size) {
+			theText = texts [pointer];
+			theTextBox.ReloadScript (theText);
+			theTextBox.currentLine = startLine;
+			theTextBox.endAtLine = endLine;
+			Invoke ("enable", 0.1f);
+		}
+		if (loop) {
+			if (pointer < texts.Length) {
+				pointer = (pointer + 1) % (texts.Length);
+			}
+		} else  {
+			pointer++;
+		}
+			
 	}
 
 	public void enable(){
