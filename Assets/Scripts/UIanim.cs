64fedd0952aabd1d5175bf675a5519e4ea601ac1
Shrey369 <sswades@gmail.com>
2016-03-29 21:54:50 -0700

    Adding new stuff

---
+++ Assets/Scripts/UIanim.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIanim : MonoBehaviour
+{
+
+    public Animator anim;
+
+	public void onPlay()
+    {
+        anim.SetBool("NearPlay", true);
+    }
+
+    public void offPlay()
+    {
+        anim.SetBool("NearPlay", false);
+    }
+
+    public void onExit()
+    {
+        anim.SetBool("NearExit", true);
+    }
+
+    public void offExit()
+    {
+        anim.SetBool("NearExit", false);
+    }
+
+
+}
