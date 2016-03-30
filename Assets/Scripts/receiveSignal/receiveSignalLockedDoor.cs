93a98ac5354eae8fa2974e8652ab16912ed91268
jayfriso <jayfriso@gmail.com>
2016-03-30 01:55:45 -0700

    Added Changes to scene 1b for the door - added door animation - added condition called isOpen to the sceneSwitcher to check if the door is open - fixed again the audio stuff being deleted from the scenes

---
+++ Assets/Scripts/receiveSignal/receiveSignalLockedDoor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class receiveSignalLockedDoor : receiveSignal {
+
+    private Animator animator;
+    //Currently I am just activating/deactivating a bool that must be on in order to swtich scenes. There is probably
+    //a better way to do this though.
+    public switchScene doorToNextLevel;
+
+    void Start() {
+        animator = GetComponent<Animator>();
+    }
+
+
+    public override void activate() {
+        animator.SetBool("open", true);
+        doorToNextLevel.setOpen(true);
+        
+    }
+
+    public override void deactivate() {
+        animator.SetBool("open", false);
+        doorToNextLevel.setOpen(false);
+
+        Debug.Log("DIACTIVATED BIATCH!!!");
+    }
+}
