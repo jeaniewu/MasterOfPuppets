6deec168d1e68cb0d29ec51d9e90f643f01ef0f4
Jeanie Suparman <jeanie.wu@hotmail.com>
2016-04-05 19:49:16 -0700

    fix Gowtham's glitch

---
+++ Assets/Scripts/adjustSize.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class adjustSize : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		GameObject levelManager = GameObject.FindGameObjectWithTag ("LevelManager");
+		int radius = levelManager.GetComponent<DollManager> ().radius;
+		transform.localScale = new Vector3(radius, radius, radius);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
