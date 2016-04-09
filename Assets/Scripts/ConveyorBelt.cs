e4e325f1b7b953814edcdad5837ed82a8528858f
jayfriso <jayfriso@gmail.com>
2016-04-09 15:50:31 -0700

    Changes for Scene 3b. - Changed length and size of conveyor belts
    Changed the implementation of conveyor belt trigger.
    --> Set so trigger finds all child covneyor belts and calls switch methods.
    
    Also added a prefab for exitdoor.

--- Assets/Scripts/ConveyorBelt.cs
+++ Assets/Scripts/ConveyorBelt.cs
@@ -4,20 +4,10 @@ using System.Collections;
 public class ConveyorBelt : MonoBehaviour {
 
 	public string direction;
-	public bool isDirChange;
 	public int speed;
+    private bool isSwitched;
 
-	// Use this for initialization
-	void Start () {
-		isDirChange = false;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-	}
-
-	void OnTriggerStay2D(Collider2D other) {
+    void OnTriggerStay2D(Collider2D other) {
 		if (other.CompareTag("Doll") || other.CompareTag("Player")){
 			if (direction == "up"){
 				other.gameObject.transform.position += new Vector3 (0, 1,0) * Time.deltaTime * speed;
@@ -29,6 +19,24 @@ public class ConveyorBelt : MonoBehaviour {
 				other.gameObject.transform.position += new Vector3 (-1, 0,0)* Time.deltaTime * speed;
 			}
 		}
-
 	}
+
+    public void setConveyorBeltDirection(bool switched) {
+        if (isSwitched != switched) {
+            isSwitched = switched;
+            direction = swap(direction);
+        }
+    }
+
+    private string swap(string dir) {
+        if (dir.Equals("right")) {
+            return "left";
+        } else if (dir.Equals("left")) {
+            return "right";
+        } else if (dir.Equals("up")) {
+            return "down";
+        } else {
+            return "up";
+        }
+    }
 }
