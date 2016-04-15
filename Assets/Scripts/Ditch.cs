000341949167c96a7987021e7dded8653c79bddd
Jeanie Suparman <jeanie.wu@hotmail.com>
2016-04-14 22:31:34 -0700

    added fall and restart functionality

---
+++ Assets/Scripts/Ditch.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Ditch : MonoBehaviour {
+
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.CompareTag ("Player")) {
+			player = other.gameObject;
+			StartCoroutine ("Fall");
+		}
+	}
+
+	IEnumerator Fall() {
+		while (player.transform.localScale.x >= 0) {
+			player.transform.Rotate (new Vector3(0,0,30));
+			player.transform.localScale -= new Vector3(0.05F, 0.05F, 0);
+			yield return null;
+		}
+		SceneManager.LoadScene (Application.loadedLevel);
+	}
+}
