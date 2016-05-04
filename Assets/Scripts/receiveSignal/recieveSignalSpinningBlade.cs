99a3873c90a949dbc9c4a79d4482ddbe3d56937f
Gowtham100 <gowtham100mohan@gmail.com>
2016-05-04 00:02:12 -0800

    redone 4a draft1

--- Assets/Scripts/receiveSignal/recieveSignalSpinningBlade.cs
+++ Assets/Scripts/receiveSignal/recieveSignalSpinningBlade.cs
@@ -1,68 +1,34 @@
 ﻿using UnityEngine;
-using System.Collections;
+
 
 public class recieveSignalSpinningBlade : receiveSignal 
 {
 
-    //private Collider2D collider2D2;
-    // private Renderer renderer2;
-   public  SpinningBlade spinningBlade;
    public GameObject spinningSawblade;
    public float rotationSpeed;
-   public bool isRight;
-
-
+   public float ROTATIONSIDE;
 
     // Use this for initialization
     void Start()
     {
-
-
-        
-
+        ROTATIONSIDE = -90;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isRight)
-        {
-            Spingright();
-        }
-        if (!isRight)
-        {
-            Spinleft();
-        }
-    }
-
-    public void Spingright()
-    {
-        transform.Rotate(Vector3.forward * 90, Time.deltaTime * rotationSpeed);
-
-    }
-
-    public void Spinleft()
-    {
-        transform.Rotate(Vector3.forward * -90, Time.deltaTime * rotationSpeed);
-
-    }
-
-    public void rotationSide(bool right)
-    {
-        isRight = right;
+        transform.Rotate(Vector3.forward * ROTATIONSIDE, Time.deltaTime * rotationSpeed);
+    
     }
 
     public override void activate()
     {
-
-        rotationSide(false);
-        
-
+        ROTATIONSIDE = 90;
     }
 
     public override void deactivate()
     {
-        rotationSide(true);
+        ROTATIONSIDE = -90;
     }
 
   
