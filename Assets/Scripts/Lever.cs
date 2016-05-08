907a83b95506be64b24f9407e6bcbf8fa5e98ed3
Jeanie Suparman <jeanie.wu@hotmail.com>
2016-05-08 14:00:00 -0700

    level 3 playable

---
+++ Assets/Scripts/Lever.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lever : MonoBehaviour {
+
+	private bool isOn;
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		isOn = false;
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		isOn = GetComponent<Trigger> ().isOn;
+		anim.SetBool ("isOn", isOn);
+	}
+}
--- Assets/Scripts/receiveSignal/receiveSignalLockedDoor.cs
+++ Assets/Scripts/receiveSignal/receiveSignalLockedDoor.cs
@@ -6,15 +6,19 @@ public class receiveSignalLockedDoor : receiveSignal {
     private Animator animator;
     //Currently I am just activating/deactivating a bool that must be on in order to swtich scenes. There is probably
     //a better way to do this though.
+	private Collider2D collider;
+
     public switchScene doorToNextLevel;
 
     void Start() {
         animator = GetComponent<Animator>();
+		collider = GetComponent <BoxCollider2D> ();
     }
 
 
     public override void activate() {
         animator.SetBool("open", true);
+		collider.enabled = false;
         if (doorToNextLevel != null) {
             doorToNextLevel.setOpen(true);
         }
@@ -22,6 +26,7 @@ public class receiveSignalLockedDoor : receiveSignal {
 
     public override void deactivate() {
         animator.SetBool("open", false);
+		collider.enabled = true;
         if (doorToNextLevel != null) {
             doorToNextLevel.setOpen(false);
         }
