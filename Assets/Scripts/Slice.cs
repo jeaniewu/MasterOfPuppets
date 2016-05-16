47778ab576a32e1734d3d54a32b751be57a55e0e
Gowtham100 <gowtham100mohan@gmail.com>
2016-05-16 03:09:22 -0800

    lvl 4 proto done

---
+++ Assets/Scripts/Slice.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Slice : MonoBehaviour
+{
+
+
+    private GameObject doll;
+    private GhostSwitchManager manager;
+    private Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        manager = GameObject.FindGameObjectWithTag("LevelManager").GetComponent<GhostSwitchManager>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        doll = other.gameObject;
+        if (/*doll.gameObject.CompareTag("Player")*/ other.tag == "Player")
+        {
+            doll.GetComponent<Controller2>().enabled = false;
+            StartCoroutine("SoulSlice");
+        }
+
+        else if ( /*doll.gameObject.CompareTag("Doll")*/ other.tag == "Doll")
+        {
+
+            doll.GetComponent<Controller2>().enabled = false;
+            StartCoroutine("NoSoulSlice");
+
+
+        }
+
+       
+    }
+ 
+
+    IEnumerator SoulSlice()
+    {
+        AnimatorClipInfo[] infos = this.anim.GetCurrentAnimatorClipInfo(0);
+        AnimatorClipInfo currentClipInfo = infos[0];
+        AnimationClip currentClip = currentClipInfo.clip;
+        float currentCliplength = currentClip.length;
+
+        
+
+
+            doll.GetComponent<Animator>().SetBool("isSoulSliced", true);
+
+
+            yield return new WaitForSeconds(currentCliplength-2);
+            manager.dollsUpdated = false;
+            manager.updateDolls();
+            SceneManager.LoadScene(Application.loadedLevel);
+
+
+        
+    }
+
+    IEnumerator NoSoulSlice()
+    {
+        AnimatorClipInfo[] infos = this.anim.GetCurrentAnimatorClipInfo(0);
+        AnimatorClipInfo currentClipInfo = infos[0];
+        AnimationClip currentClip = currentClipInfo.clip;
+        float currentCliplength = currentClip.length;
+
+  
+            doll.GetComponent<Animator>().Play("soulless-sliced");
+
+            yield return new WaitForSeconds(currentCliplength - 2);
+
+            if (doll.tag == "Doll")
+            {
+                doll.SetActive(false);
+            }
+
+
+ 
+        }
+    }   
+
+
