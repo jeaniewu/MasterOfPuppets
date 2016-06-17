86847c1af048603b21d504074b20b6c0f1e171d7
jfriso <jayfriso@gmail.com>
2016-06-16 22:09:09 -0700

    Added a Scene Select Menu for trailer recording and testing purposes.

---
+++ Assets/SceneSkip.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneSkip : MonoBehaviour {
+   
+
+    public void sceneSwitch(string scene) {
+        SceneManager.LoadScene(scene);
+    }
+
+}
