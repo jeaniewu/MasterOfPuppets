4de7b3f35d865e012aabf909350803122cd7fe62
jayfriso <jayfriso@gmail.com>
2016-06-19 15:54:34 -0700

    Commiting final animation changes for multi colored dolls. Now using Animation Override Controller. Also had to add a soulSlice and soullessSliceReference animation that is the same between all colors of doll to in order for the part of code that calculates the animation time to work.

--- Assets/Scripts/dollType.cs
+++ Assets/Scripts/dollType.cs
@@ -5,18 +5,5 @@ public class dollType : MonoBehaviour {
 
 	public string type;
     public int dollColor = 0;
-    private Animator animator;
-
-	// Use this for initialization
-	void Start () {
-        animator = GetComponent<Animator>();
-        animator.SetInteger("Color", dollColor);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
 
 }
