08d2f7959d250eb341f8a7249c14fd09bffd7b95
Jeanie Suparman <jeanie.wu@hotmail.com>
2016-06-26 20:29:03 -0700

    Ui fix (#76)
    * openScene UI Fix
    
    * openScene
    
    * improve ghost Switch tutorial
    
    * tutorial UI only played once
    
    * use dictionary for object triggers

---
+++ Assets/Scripts/checkOneTimeTrigger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class checkOneTimeTrigger : MonoBehaviour {
+
+	public bool hasBeenTriggeredOnce;
+
+	// Use this for initialization
+	void Start () {
+		UpdateTrigger ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void UpdateTrigger () {
+		GameManager.getInstance ().objectsTriggered.TryGetValue (gameObject.name, out hasBeenTriggeredOnce);
+	}
+
+	public void setTriggered(){
+		GameManager.getInstance ().objectsTriggered.Add (gameObject.name, true);
+		UpdateTrigger ();
+	}
+}
