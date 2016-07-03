28642003098345c3fab0084a0de32142ee9ba4d4
jayfriso <jayfriso@gmail.com>
2016-07-02 20:40:33 -0700

    Commiting progress for modifications based on Full-Indie feedback. - Open Scene removed door - Added interact UI to 1b secret item.

---
+++ Assets/Scripts/Trigger/UITriggerForOneB.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UITriggerForOneB : UITrigger {
+
+    public SpriteRenderer[] interactButtons;
+
+    public override void interact() {
+        disableInteractUI();
+        manager.EnableTextBox();
+        StartCoroutine(WaitForTextBoxInput());
+    }
+
+    IEnumerator WaitForTextBoxInput() {
+        yield return new WaitForSeconds(0.5f);
+        gameObject.layer = 0;
+        bool textBoxClosed = false;
+        while (!Input.GetButtonDown("Interact")) {
+            yield return null;
+            Debug.Log("Still waiting");
+        }
+        yield return enableSecretMessage();
+    }
+
+    //Get rid of the tutorial interact UI
+    private void disableInteractUI() {
+        gameObject.layer = 0;
+        //disable all the interact sprites
+        foreach(SpriteRenderer interactButton in interactButtons) {
+            interactButton.enabled = false;
+        }
+    }
+}
