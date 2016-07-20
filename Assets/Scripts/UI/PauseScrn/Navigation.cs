d7119092c652bc484759656092c9b338ccd28ce2
Gowtham Mohan <gowtham100mohan@gmail.com>
2016-07-20 17:49:34 +0530

    changing to uinty UI

--- Assets/Scripts/UI/PauseScrn/Navigation.cs
+++ Assets/Scripts/UI/PauseScrn/Navigation.cs
@@ -5,149 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class Navigation : MonoBehaviour
 {
-
-    public Text RESUME;
-    public Text NOTEBOOK;
-    public Text OPTION;
-    public Text QUIT;
-    public Button BackButton;
-    public GameObject OptionsOption;
+	public Button btnResume;
 	private TextBoxManager manager;
-    //private Text[] menuOptions;
-    public int selectedOption;
+	public GameObject OptionsOption;
+	public Navigation_Notebook notebookManager;
 
-    public Navigation_Notebook notebookManager;
-
-
-    // Use this for initialization
-    void Start()
-    {
-        ResumeSelected();
+	// Use this for initialization
+	void Start () {
+		btnResume.Select ();
 		manager = GameObject.FindGameObjectWithTag ("TextBoxManager").GetComponent<TextBoxManager> ();
-        OptionsOption.SetActive(false);
-        //menuOptions = GetComponentsInChildren<Text>();
-    }
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        //Debug.Log("Length of options:" + menuOptions.Length);
+	public void selectResume(){
+		gameObject.SetActive(false);
+		manager.enablePlayer ();
+	}
 
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            selectedOption++;
-            Debug.Log("Option is" + selectedOption);
-        }
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            selectedOption--;
-            Debug.Log("Option is" + selectedOption);
-        }
+	public void selectNotebook(){
+		notebookManager.startNotebook();
+		gameObject.SetActive(false);
+	}
 
-
-        if (selectedOption <= 3 && selectedOption >= 0)
-        {
-            if (selectedOption == 0)
-            {
-                ResumeSelected();
-                OptionsDeSelected();
-                NotebookDeSelected();
-                if (Input.GetButtonDown("Interact"))
-                {
-                    gameObject.SetActive(false);
-					manager.enablePlayer ();
-                    Debug.Log("pressed");
-                }
-            }
-            if (selectedOption == 1)
-            {
-                ResumeDeSelected();
-                OptionsDeSelected();
-                NotebookSelected();
-                if (Input.GetButtonDown("Interact"))
-                {
-                    notebookManager.startNotebook();
-                    gameObject.SetActive(false);
-                }
-            }
-            if (selectedOption == 2)
-            {
-                NotebookDeSelected();
-                QuitDeSelected();
-                OptionsSelected();
-                if (Input.GetButtonDown("Interact"))
-                {
-                    OptionsOption.SetActive(true);
-                    Debug.Log("pressed");
-                    gameObject.SetActive(false);
-                }
-            }
-            if (selectedOption == 3)
-            {
-                OptionsDeSelected();
-                ResumeDeSelected();
-                QuitSelected();
-                if (Input.GetButtonDown("Interact"))
-                {
-					SceneManager.LoadScene("NewTitleScene");
-                }
-            }
-        }
-        else if (selectedOption > 2)
-        {
-            QuitDeSelected();
-            ResumeSelected();
-            NotebookDeSelected();
-            selectedOption = 0;
-        }
-        else if (selectedOption < 0)
-        {
-            ResumeDeSelected();
-            QuitSelected();
-            selectedOption = 3;
-        }
-
-    }
-
-
-
-    void ResumeSelected()
-    {
-        RESUME.color = Color.red;
-
-    }
-
-    public void NotebookSelected()
-    {
-        NOTEBOOK.color = Color.red;
-    }
-
-    void OptionsSelected()
-    {
-        OPTION.color = Color.red;
-    }
-
-    void QuitSelected()
-    {
-        QUIT.color = Color.red;
-    }
-    void ResumeDeSelected()
-    {
-        RESUME.color = Color.white;
-    }
-
-    void NotebookDeSelected()
-    {
-        NOTEBOOK.color = Color.white;
-    }
-
-    void OptionsDeSelected()
-    {
-        OPTION.color = Color.white;
-    }
-
-    void QuitDeSelected()
-    {
-        QUIT.color = Color.white;
-    }
+	public void selectOptions(){
+		OptionsOption.SetActive(true);
+		gameObject.SetActive(false);
+	}
+	public void selectQuit(){
+		SceneManager.LoadScene("NewTitleScene");
+	}
 }
